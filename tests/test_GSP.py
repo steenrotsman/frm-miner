da42b50e0897e17af373647c612355bcf404152c
Stijn <s.j.rotman@uvt.nl>
2022-12-16 11:19:58 +0100

    Add longest common substring test.

--- tests/test_GSP.py
+++ tests/test_GSP.py
@@ -5,6 +5,15 @@ from motifminer.preprocessing import sax
 from .test_data import ts, rag
 
 class TestMotif(unittest.TestCase):
+    def test_lcs(self):
+        gsp = GSP([])
+        commons = ['a' * i for i in range(0, 100, 5)]
+        for i, common in enumerate(commons):
+            p1 = 'abc' + common + 'abc'
+            p2 = 'def' + common + 'def'
+
+            self.assertEqual(len(common), gsp.lcs(p1, p2, len(p1), len(p2)))
+
     def test_mine(self):
         seq = sax(ts, 1, 3)
         gsp = GSP(seq)
