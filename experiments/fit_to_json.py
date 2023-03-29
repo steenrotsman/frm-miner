cf7dc0017ae4e98f7c99b92d255d5a4329ebc10a
Stijn <s.j.rotman@uvt.nl>
2023-03-29 12:31:23 +0200

    Improve experiments.

--- experiments/fit_to_json.py
+++ experiments/fit_to_json.py
@@ -13,7 +13,6 @@ MIN_ACTIVITY_LEN = 300
 
 def main():
     records = get_records(FIT_DIR)
-    records = parse_records(records)
     write_records(records)
 
 
@@ -27,27 +26,21 @@ def get_records(directory):
             for frame in fit:
                 # Only keep frame data, no personal or metadata
                 if frame.frame_type == fitdecode.FIT_FRAME_DATA and frame.name == 'record':
-                    # JSON can't hold data with timestamp type
-                    record.append({f.name: f.value if f.name != 'timestamp' else str(f.value) for f in frame.fields})
+                    rec = {}
+                    for f in frame.fields:
+                        # Don't keep coordinates for privacy
+                        if f.name in ['position_lat', 'position_long']:
+                            continue
 
+                        # JSON can't hold data with timestamp type; convert to str
+                        if f.name == 'timestamp':
+                            rec[f.name] = str(f.value)
+                        else:
+                            rec[f.name] = f.value
+                    record.append(rec)
             records.append(record)
 
     return list(filter(lambda rec: len(rec) > MIN_ACTIVITY_LEN, records))
-
-
-def parse_records(records):
-    """Convert timestamps to strings and coordinates to lat-longs."""
-    for i, record in enumerate(records):
-        for j, rec in enumerate(record):
-            # Convert timestamp to string
-            records[i][j]['timestamp'] = str(rec['timestamp'])
-
-            # Convert lat and long to float
-            for field in ['position_lat', 'position_long']:
-                # https://gis.stackexchange.com/a/371667
-                records[i][j][field] = rec[field] / 11930465
-
-    return records
 
 
 def write_records(records):
--- experiments/quantitative.py
+++ experiments/quantitative.py
@@ -18,7 +18,7 @@ UNITS = 100
 TS_LEN = 10000
 MOTIF_LEN = 500
 NOISE_LEVELS = [0.1, 0.3, 0.5, 0.7, 0.9]
-INJECT = 75
+INJECT = [100, 75]
 
 # Parameters
 MINSUP = 0.3
@@ -29,7 +29,7 @@ MAX_OVERLAP = 0.8
 LOCAL = True
 K = 3
 
-np.random.seed(123)
+np.random.seed(0)
 
 
 def main():
@@ -38,28 +38,23 @@ def main():
     plot_motif(motif, NOISE_LEVELS)
     plot_example(noise[:5], motif, 0.5)
 
-    motifs = []
-    overlaps = []
-    consensus_motifs = []
-    for noise_level in NOISE_LEVELS:
-        # Put noisy motif into data
-        data, locations = get_data(noise, motif, noise_level, INJECT)
+    for inject in INJECT:
+        motifs = []
+        consensus_motifs = []
+        for noise_level in NOISE_LEVELS:
+            # Put noisy motif into data
+            data, locations = get_data(noise, motif, noise_level, inject)
 
-        # Mine motifs of variable length
-        mm = Miner(data, MINSUP, SEGLEN, ALPHABET, MIN_LEN, MAX_OVERLAP, LOCAL, K)
-        top_motifs = mm.mine_motifs()
-        motifs.append(top_motifs)
+            # Mine motifs of variable length
+            mm = Miner(data, MINSUP, SEGLEN, ALPHABET, MIN_LEN, MAX_OVERLAP, LOCAL, K)
+            top_motifs = mm.mine_motifs()
+            motifs.append(top_motifs)
 
-        # Find if occurrences of top_motifs correspond to actual locations
-        overlap = get_overlap(top_motifs, locations)
-        overlaps.append(overlap)
+            # consensus_motifs.append(ostinato(data, MOTIF_LEN))
 
-        consensus_motifs.append(ostinato(data, MOTIF_LEN))
-
-    fig, axs = plt.subplots(nrows=len(motifs[0]), layout='compressed', ncols=len(NOISE_LEVELS), sharey='all', sharex='all')
-    plot_motifs(fig, chain.from_iterable(axs.T), chain.from_iterable(motifs), ALPHABET, MOTIF_LEN)
-    print(np.array(overlaps).T)
-    plot_ostinato(consensus_motifs)
+        fig, axs = plt.subplots(nrows=len(motifs[0]), layout='compressed', ncols=len(NOISE_LEVELS), sharey='all', sharex='all')
+        plot_motifs(fig, chain.from_iterable(axs.T), chain.from_iterable(motifs), ALPHABET, MOTIF_LEN)
+        # plot_ostinato(consensus_motifs)
 
 
 def get_motif(length):
@@ -91,29 +86,6 @@ def get_data(noise, motif, noise_level, inject):
         data.append(row)
 
     return data, locations
-
-
-def get_overlap(top_motifs, locations):
-    overlap = []
-    for motif in top_motifs:
-        success = 0
-        for i, (start, end) in motif.match_indexes.items():
-            true_start = locations[i]
-            true_end = locations[i] + MOTIF_LEN
-
-            # Check if match doesn't completely fall outside of range
-            if true_start < 0 or start > true_end or end < true_start:
-                continue
-
-            if start < true_start:
-                success += end - true_start > true_start - start
-            elif end > true_end:
-                success += end - true_end < true_end - start
-            else:
-                success += True
-        overlap.append(success / len(motif.match_indexes))
-
-    return overlap
 
 
 def plot_motif(motif, noise_levels):
