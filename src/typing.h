c195200e322ccf81b4c110243883c3e2f17d9964
Stijn <s.j.rotman@uvt.nl>
2023-06-12 17:42:55 +0200

    Change c++ files to symlinks.

--- src/typing.h
+++ src/typing.h
@@ -1,15 +1,1 @@
-//
-// Created by stijn on 6/8/23.
-//
-
-#ifndef FRM_C_TYPING_H
-#define FRM_C_TYPING_H
-
-#include <vector>
-#include <string>
-
-using Pattern = std::vector<int>;
-using DiscreteDB = std::vector<std::vector<int>>;
-using TimeSeriesDB = std::vector<std::vector<double>>;
-
-#endif //FRM_C_TYPING_H
+/home/stijn/Documents/code/frm-c/src/typing.h
