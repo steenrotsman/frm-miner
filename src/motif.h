638029bfb084f36427cbfe67aedc096c9f0d7957
Stijn <s.j.rotman@uvt.nl>
2023-06-14 10:06:14 +0200

    Change symlinks back to c++ files.

--- src/motif.h
+++ src/motif.h
@@ -1,1 +1,41 @@
-/home/stijn/Documents/code/frm-c/src/motif.h
+//
+// Created by stijn on 6/8/23.
+//
+
+#ifndef FRM_C_MOTIF_H
+#define FRM_C_MOTIF_H
+
+#include <vector>
+#include <map>
+#include <unordered_map>
+
+#include "typing.h"
+
+class Motif {
+private:
+    Pattern pattern;
+    int length;
+    std::unordered_map<int, std::vector<int>> indexes;
+    std::unordered_map<int, std::vector<double>> average_occurrences;
+    std::vector<double> representative;
+    std::unordered_map<int, int> best_matches;
+    double rmse;
+
+    void set_average_occurrences(TimeSeriesDB& timeseries);
+    void set_representative();
+    void set_best_matches_and_rmse(TimeSeriesDB &timeseries);
+public:
+    explicit Motif(Pattern pattern);
+
+    void record_index(int seq, int idx_in_seq);
+
+    Pattern get_pattern() { return pattern; };
+    std::unordered_map<int, std::vector<int>> get_indexes() { return indexes; };
+    std::vector<double> get_representative() { return representative; };
+    std::unordered_map<int, int> get_best_matches() { return best_matches; };
+    double get_rmse() const { return rmse; };
+
+    void map(TimeSeriesDB& timeseries, int seglen);
+};
+
+#endif //FRM_C_MOTIF_H
--- src/sax.h
+++ src/sax.h
@@ -1,1 +1,17 @@
-/home/stijn/Documents/code/frm-c/src/sax.h
+//
+// Created by stijn on 6/8/23.
+//
+
+#ifndef FRM_C_SAX_H
+#define FRM_C_SAX_H
+
+#include <vector>
+
+#include "typing.h"
+
+DiscreteDB sax(TimeSeriesDB & ts, int seglen, int alphabet);
+int get_discrete_value(int alphabet, double segmean);
+void znorm(std::vector<std::vector<double>>& ts);
+
+
+#endif //FRM_C_SAX_H
