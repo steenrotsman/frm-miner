cf75bc037503e2f5142243e87baa9e5b19e9a548
Stijn <s.j.rotman@uvt.nl>
2023-12-21 21:13:15 +0100

    Implement k of P Ostinato.

--- experiments/ostinato.py
+++ experiments/ostinato.py
@@ -8,10 +8,10 @@ with warnings.catch_warnings():
 
 
 def ostinato(ts, m):
-    k = len(ts) - 1
+    n = len(ts)
     bsf_rad, ts_index, ss_index = np.inf, 0, 0
     for j, _ in enumerate(ts):
-        h = j + 1 if j < k else 0
+        h = (j + 1) % n
         profile, _ = mp.abjoin(ts[j], ts[h], m)
         for q in np.argsort(profile):
             radius = profile[q]
@@ -19,9 +19,38 @@ def ostinato(ts, m):
                 break
             for i, _ in enumerate(ts):
                 if i not in (j, h):
-                    radius = max(radius, min(mass(ts[i], ts[j][q:q+m])))
+                    radius = max(radius, min(mass(ts[i], ts[j][q:q+m])).real)
                     if radius >= bsf_rad:
                         break
             if radius < bsf_rad:
                 bsf_rad, ts_index, ss_index = radius, j, q
     return bsf_rad, ts_index, ss_index
+
+
+def k_of_p_ostinato(ts, m, k):
+    n = len(ts)
+    if n == k:
+        return ostinato(ts, m)
+
+    bsf_rad, ts_index, ss_index, bsf_outliers = np.inf, 0, 0, {}
+    for j, _ in enumerate(ts):
+        h = (j + 1) % n
+        profile, _ = mp.abjoin(ts[j], ts[h], m)
+        for q in np.argsort(profile):
+            radius = 0
+            outliers = {h: profile[q]}
+
+            for i, _ in enumerate(ts):
+                if i not in (j, h):
+                    rad = min(mass(ts[i], ts[j][q:q+m])).real
+                    if rad > radius:
+                        outliers[i] = rad
+                        if len(outliers) > n-k:
+                            smallest_outlier_index, smallest_outlier_radius = min(outliers.items(), key=lambda x: x[1])
+                            radius = smallest_outlier_radius
+                            outliers.pop(smallest_outlier_index)
+                            if radius > bsf_rad:
+                                break
+            if radius < bsf_rad:
+                bsf_rad, ts_index, ss_index, bsf_outliers = radius, j, q, outliers
+    return bsf_rad, ts_index, ss_index, bsf_outliers
