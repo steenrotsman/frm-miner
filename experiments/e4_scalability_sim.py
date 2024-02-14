2456b476ad4ffa17bf637f7b8c28ec0ced5c54c9
Stijn <s.j.rotman@uvt.nl>
2024-02-14 15:33:29 +0100

    Rename scalability experiments and profile memory scalability.

---
+++ experiments/e4_scalability_sim.py
@@ -0,0 +1,129 @@
+from collections import defaultdict
+from itertools import product
+from os.path import join
+from statistics import fmean
+from time import perf_counter
+
+import matplotlib.pyplot as plt
+import numpy as np
+from e1_runtime import benchmark_miner
+from memory_profiler import memory_usage
+from plot import remove_spines
+
+TIME_FILE = 'e4_scalability sim.csv'
+SPACE_FILE = 'e4_scalability sim memory.csv'
+NAME = '4 scalability sim'
+
+MINSUP = 0.3
+SEGLEN = 10
+ALPHABET = 5
+
+LENGTHS = [10**i for i in range(1, 5)]
+ROWS = [10**i for i in range(1, 5)]
+INJECT = 40
+ITER = 10
+
+RNG = np.random.default_rng(0)
+
+
+def main():
+    experiment(TIME_FILE, 'Seconds')
+    experiment(SPACE_FILE, 'Bytes')
+
+
+def experiment(file, measure):
+    seen = get_seen(file)
+    for setting in product(LENGTHS, ROWS, range(ITER)):
+        if setting not in seen:
+            simulation(*setting, file, measure)
+    results = get_results(file)
+    plot_results(*results, NAME, measure)
+
+
+def get_seen(file):
+    with open(file) as fp:
+        seen = []
+        for row in fp.readlines():
+            length, rows, i = tuple(row.split(',')[:3])
+            seen.append((int(length), int(rows), int(i)))
+    return seen
+
+
+def simulation(length, rows, iter, file, measure):
+    name = f'{length} {rows} {iter} ({measure})'
+    print(f'{name}...')
+    data = get_data(length, rows)
+
+    if measure == 'Bytes':
+        result = memory_usage((benchmark_miner, (data,)), max_usage=True)
+    else:
+        start = perf_counter()
+        benchmark_miner(data)
+        end = perf_counter()
+        result = end - start
+
+    with open(file, 'a') as fp:
+        fp.write(f'{length},{rows},{iter},{result}\n')
+    print(f'{name} done!')
+
+
+def get_data(length, rows):
+    data = RNG.standard_normal((rows, length))
+    motif_length = length // 10
+    motif = RNG.standard_normal(motif_length)
+    inject = RNG.integers(1, length - motif_length, rows)
+    inject *= RNG.integers(0, 100, rows) < INJECT
+    for i, idx in enumerate(inject):
+        if idx:
+            data[i][idx : idx + motif_length] = motif
+    return data
+
+
+def get_results(file):
+    lengths = defaultdict(list)
+    rows = defaultdict(list)
+    total = defaultdict(list)
+
+    with open(file) as fp:
+        for row in fp.readlines():
+            length, row, iter, runtime = tuple(row.split(','))
+            length, row, runtime = int(length), int(row), float(runtime)
+            lengths[length].append(runtime)
+            rows[row].append(runtime)
+            total[row * length].append(runtime)
+
+    lengths = {size: fmean(x) for size, x in lengths.items()}
+    rows = {size: fmean(x) for size, x in rows.items()}
+    total = {size: fmean(x) for size, x in total.items()}
+
+    return lengths, rows, total
+
+
+def plot_results(lengths, rows, total, name, measure, marker='.', ls='-'):
+    fig, axs = plt.subplots(nrows=2, sharey='all', layout='constrained')
+
+    # Plot the lines for number of rows, row length, and total data size
+    params = {'marker': marker, 'ls': ls, 'ms': 3, 'lw': 1}
+    axs[0].plot(*zip(*sorted(rows.items())), **params)
+    axs[0].plot(*zip(*sorted(lengths.items())), **params)
+
+    # https://stackoverflow.com/a/63789660
+    cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    axs[1].plot(*zip(*sorted(total.items())), **params, color=cycle[2])
+
+    # Set the x-axis and y-axis scales to logarithmic
+    for ax in axs:
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        remove_spines(ax, False)
+
+    axs[0].set_ylabel(measure, labelpad=0).set_y(-0.5)
+    axs[1].set_xlabel('Size')
+
+    plt.savefig(join('figs', f'{name} {measure}.eps'))
+    plt.savefig(join('figs', f'{name} {measure}.png'))
+    plt.close()
+
+
+if __name__ == '__main__':
+    main()
