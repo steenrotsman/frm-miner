988733d4a4e9eb8daf7d55d4e13aa7c72f473356
Stijn <s.j.rotman@uvt.nl>
2024-02-14 15:32:10 +0100

    Change default colors.

--- experiments/plot.py
+++ experiments/plot.py
@@ -6,10 +6,11 @@ from cycler import cycler
 
 WIDTH = 0.0138 * 347.12354 / 2
 HEIGHT = WIDTH / 2
-colors = cycler(color=['blue', 'tomato', 'gold', 'teal'])
+colors = cycler(color=['blue', 'darkkhaki', 'chocolate', 'mediumseagreen'])
 params = {
     'axes.labelsize': 6,
     'axes.prop_cycle': colors,
+    'figure.constrained_layout.use': True,
     'figure.figsize': [WIDTH, HEIGHT],
     'font.family': 'serif',
     'font.size': 6,
@@ -49,3 +50,14 @@ def remove_spines(ax, remove_y=True):
         ax.spines['left'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
+
+
+if __name__ == '__main__':
+    from PIL import Image
+
+    for i in range(4):
+        plt.plot([i, i])
+    plt.savefig(join('figs', 'colors.png'))
+    img = Image.open(join('figs', 'colors.png'))
+    img_gray = img.convert('L')
+    img_gray.save(join('figs', 'grayscale.png'))
