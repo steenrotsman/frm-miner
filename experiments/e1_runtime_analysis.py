2ada5b721b4000ca22a5752cb6dd65d91983612d
Stijn <s.j.rotman@uvt.nl>
2024-03-15 13:58:53 +0100

    Improve runtime plots

--- experiments/e1_runtime_analysis.py
+++ experiments/e1_runtime_analysis.py
@@ -1,60 +1,106 @@
 from collections import defaultdict
+from math import ceil, floor, log10
 from os.path import join
 
 import matplotlib.pyplot as plt
 from plot import WIDTH
 
-FILE = 'e1_runtime.csv'
 SIZE = 3
-X_METHOD = 'benchmark_stumpy'
-Y_METHOD = 'benchmark_py_miner_new'
-X_NAME = 'Ostinato'
-Y_NAME = 'FRM-Miner'
 
-x_method_runtimes = {}
-y_method_runtimes = {}
-runtimes = defaultdict(float)
-with open(FILE) as fp:
-    for line in fp.readlines():
-        row = line[:-1].split(',')
-        method, dataset, runtime = row
-        runtime = float(runtime)
-        runtimes[method] += runtime
+SETTINGS = [
+    # file, x_method, y_method, x_name, y_name, lines, fn
+    (
+        'e1_runtime.csv',
+        'benchmark_py_miner_1_2',
+        'benchmark_miner_2_2',
+        'FRM-Miner 1.0',
+        'FRM-Miner 2.0',
+        3,
+        'frm runtime',
+    ),
+    (
+        'e1_runtime.csv',
+        'benchmark_stumpy',
+        'benchmark_miner_2_1',
+        'Ostinato',
+        'FRM-Miner',
+        4,
+        'ostinato runtime',
+    ),
+    (
+        'e1_memory.csv',
+        'benchmark_miner_1_2',
+        'benchmark_miner_2_2',
+        'FRM-Miner 1.0',
+        'FRM-Miner 2.0',
+        3,
+        'frm memory',
+    ),
+]
 
-        if method == X_METHOD:
-            x_method_runtimes[dataset] = float(runtime)
-        elif method == Y_METHOD:
-            y_method_runtimes[dataset] = float(runtime)
 
-# Print total run times
-for method, runtime in runtimes.items():
-    print(f'{method[10:]}: {runtime/3600:.2f}')
+def main():
+    for file, *settings in SETTINGS:
+        results = get_results(file)
+        print_results(results)
+        plot_results(results, *settings)
 
-# Plot comparison of methods
-plt.figure(figsize=[WIDTH / 1.5, WIDTH / 1.5])
-plt.xscale('log')
-plt.yscale('log')
 
-# Create lists of the dictionaries, sorted on dataset name, to ensure runtimes for the same datasets are compared
-x_method_runtimes = [x[1] for x in sorted(x_method_runtimes.items())]
-y_method_runtimes = [x[1] for x in sorted(y_method_runtimes.items())]
+def get_results(file):
+    runtimes = defaultdict(dict)
+    with open(file) as fp:
+        for line in fp.readlines():
+            row = line[:-1].split(',')
+            method, dataset, runtime = row
+            runtime = float(runtime)
+            runtimes[method][dataset] = runtime
+    return runtimes
 
-plt.scatter(
-    x_method_runtimes, y_method_runtimes, s=SIZE, color='k', label='UCR data set'
-)
 
-# Add lines for equality, 10x less, and 100x less runtime
-plt.plot([0.05, 250000], [0.05, 250000], 'k', label='Equality')
-plt.plot([0.5, 250000], [0.05, 25000], 'k--', label='10x Faster')
-plt.plot([5, 250000], [0.05, 2500], 'k:', label='100x Faster')
+def print_results(runtimes):
+    for method, runtime in runtimes.items():
+        print(f'{method[10:]}: {sum(runtime.values())/3600:.2f}')
 
-# Aesthetics
-plt.xlim([10e-3, 10e5])
-plt.ylim([10e-3, 10e5])
-plt.xlabel(X_NAME)
-plt.ylabel(Y_NAME)
-plt.legend()
 
-plt.savefig(join('figs', '1 runtime.eps'))
-plt.savefig(join('figs', '1 runtime.png'))
-plt.close()
+def plot_results(runtimes, x_method, y_method, x_name, y_name, lines, fn):
+    plt.figure(figsize=[WIDTH / 1.5, WIDTH / 1.5])
+    plt.xscale('log')
+    plt.yscale('log')
+
+    # Create lists of the dictionaries, sorted on dataset name, to ensure runtimes for the same datasets are compared
+    x_method_runtimes = [x[1] for x in sorted(runtimes[x_method].items())]
+    y_method_runtimes = [x[1] for x in sorted(runtimes[y_method].items())]
+
+    lim = [
+        10 ** floor(log10(min(x_method_runtimes + y_method_runtimes))),
+        10 ** ceil(log10(max(x_method_runtimes + y_method_runtimes))),
+    ]
+    plt.scatter(
+        x_method_runtimes, y_method_runtimes, s=SIZE, color='k', label='UCR data set'
+    )
+
+    # Add lines for equality, and orders of magnitude difference
+    start = 1.5 * lim[0]
+    end = 0.7 * lim[1]
+
+    for i, line in enumerate(['k', 'k--', 'k:', 'k-.'][:lines]):
+        factor = 10**i
+        label = f'{factor}x Difference'
+        if not i:
+            label = 'Equality'
+        plt.plot([start * factor, end], [start, end / factor], line, label=label)
+
+    # Aesthetics
+    plt.xlim(lim)
+    plt.ylim(lim)
+    plt.xlabel(x_name)
+    plt.ylabel(y_name)
+    plt.legend()
+
+    plt.savefig(join('figs', f'1 {fn}.eps'))
+    plt.savefig(join('figs', f'1 {fn}.png'))
+    plt.close()
+
+
+if __name__ == '__main__':
+    main()
