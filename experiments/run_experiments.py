5b302fdb8ac0aa66fbfe7e87fbddadf1da432b6f
Stijn <s.j.rotman@uvt.nl>
2024-03-22 15:51:39 +0100

    Do not run all experiments with multiprocessing.

--- experiments/run_experiments.py
+++ experiments/run_experiments.py
@@ -1,14 +1,13 @@
 import os
 import subprocess
-from multiprocessing import Pool, cpu_count
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 def main():
     files = [file for file in os.listdir(PATH) if file[0] == 'e' and file[-3:] == '.py']
-    with Pool(cpu_count()) as p:
-        p.map(run, files)
+    for file in files:
+        run(file)
 
 
 def run(file):
