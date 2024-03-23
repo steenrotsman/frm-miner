e9683684ea62d210f4bec0d820b4a81e236568a3
Stijn <s.j.rotman@uvt.nl>
2024-03-23 21:18:24 +0100

    Adjust scalability params.

--- experiments/e2_params.py
+++ experiments/e2_params.py
@@ -10,10 +10,10 @@ FILE = 'e2_params.csv'
 
 
 # Parameter ranges for FRM-Miner
-MINSUP = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-SEGLEN = [10, 15, 20, 25, 30, 35, 40, 45, 50]
+MINSUP = [0.1, 0.3, 0.5, 0.7, 0.9]
+SEGLEN = [10, 20, 30, 40, 50]
 ALPHABET = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-OMAX = [0.6, 0.7, 0.8, 0.9, 1.0]
+OMAX = [0.5, 0.6, 0.7, 0.8, 0.9]
 N = 5
 
 data = get_stocks()
@@ -24,10 +24,10 @@ def main():
     with open(FILE) as fp:
         seen = []
         for row in fp.readlines():
-            i, m, s, a = tuple(row.split(',')[:4])
-            seen.append((int(i), float(m), int(s), int(a)))
+            i, m, s, a, o = row.split(',')[:5]
+            seen.append((int(i), float(m), int(s), int(a), float(o)))
 
-    with Pool(1, maxtasksperchild=1) as p:
+    with Pool(8, maxtasksperchild=1) as p:
         settings = [
             s
             for s in product(range(N), MINSUP, SEGLEN, ALPHABET, OMAX)
@@ -36,16 +36,17 @@ def main():
         p.starmap(mine, settings)
 
 
-def mine(i, minsup, seglen, alphabet):
-    print(f'{i},{minsup},{seglen},{alphabet}...')
+def mine(i, minsup, seglen, alpha, omax):
+    print(f'{i},{minsup},{seglen},{alpha},{omax}...')
     start = perf_counter()
-    miner = Miner(minsup, seglen, alphabet)
+    miner = Miner(minsup, seglen, alpha, max_overlap=omax)
     miner.mine(data)
     end = perf_counter()
-    print(f'{i},{minsup},{seglen},{alphabet} done!')
+    print(f'{i},{minsup},{seglen},{alpha},{omax} done!')
 
     with open(FILE, 'a') as fp:
-        fp.write(f'{i},{minsup},{seglen},{alphabet},{end-start},{len(miner.motifs)}\n')
+        time = end - start
+        fp.write(f'{i},{minsup},{seglen},{alpha},{omax},{time},{len(miner.motifs)}\n')
 
 
 if __name__ == '__main__':
--- experiments/e2_params_analysis.py
+++ experiments/e2_params_analysis.py
@@ -6,8 +6,8 @@ import plot  # noqa
 
 FILE = 'e2_params.csv'
 
-MINSUP = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-SEGLEN = [10, 15, 20, 25, 30, 35, 40, 45, 50]
+MINSUP = [0.1, 0.3, 0.5, 0.7, 0.9]
+SEGLEN = [10, 20, 30, 40, 50]
 ALPHABET = [2, 3, 4, 5, 6, 7, 8, 9, 10]
 OMAX = [0.5, 0.6, 0.7, 0.8, 0.9]
 PARAMS = ['minsup', 'seglen', 'alphabet', 'omax']
