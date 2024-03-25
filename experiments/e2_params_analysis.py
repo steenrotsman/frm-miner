988f4ec597891fa5fa6ce406fa12a722ec928c6a
Stijn <s.j.rotman@uvt.nl>
2024-03-25 12:05:00 +0100

    Add omax to parameter experiment analysis.

--- experiments/e2_params_analysis.py
+++ experiments/e2_params_analysis.py
@@ -9,7 +9,8 @@ FILE = 'e2_params.csv'
 MINSUP = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 SEGLEN = [10, 15, 20, 25, 30, 35, 40, 45, 50]
 ALPHABET = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-PARAMS = ['minsup', 'seglen', 'alphabet']
+OMAX = [0.5, 0.6, 0.7, 0.8, 0.9]
+PARAMS = ['minsup', 'seglen', 'alphabet', 'omax']
 VALUES = [MINSUP, SEGLEN, ALPHABET]
 BAR_WIDTH = 0.45
 
@@ -17,41 +18,46 @@ BAR_WIDTH = 0.45
 minsup_runtime = [[] for _ in MINSUP]
 seglen_runtime = [[] for _ in SEGLEN]
 alphabet_runtime = [[] for _ in ALPHABET]
+omax_runtime = [[] for _ in OMAX]
 minsup_n_patterns = [[] for _ in MINSUP]
 seglen_n_patterns = [[] for _ in SEGLEN]
 alphabet_n_patterns = [[] for _ in ALPHABET]
+omax_n_patterns = [[] for _ in OMAX]
 
 with open(FILE) as fp:
     for row in fp.readlines():
-        _, m, s, a, r, n = tuple(row[:-1].split(','))
+        _, m, s, a, o, r, n = tuple(row[:-1].split(','))
         m = float(m)
         s = int(s)
         a = int(a)
+        o = int(o)
         r = float(r)
         n = int(n)
 
-        if m not in MINSUP or s not in SEGLEN or a not in ALPHABET:
+        if m not in MINSUP or s not in SEGLEN or a not in ALPHABET or o not in OMAX:
             continue
 
         minsup_runtime[MINSUP.index(m)].append(r)
         seglen_runtime[SEGLEN.index(s)].append(r)
         alphabet_runtime[ALPHABET.index(a)].append(r)
+        omax_runtime[OMAX.index(o)].append(r)
         minsup_n_patterns[MINSUP.index(m)].append(n)
         seglen_n_patterns[SEGLEN.index(s)].append(n)
         alphabet_n_patterns[ALPHABET.index(a)].append(n)
+        omax_n_patterns[OMAX.index(o)].append(n)
 
 results = [
     ([fmean(x) for x in minsup_runtime], [fmean(x) for x in minsup_n_patterns]),
     ([fmean(x) for x in seglen_runtime], [fmean(x) for x in seglen_n_patterns]),
     ([fmean(x) for x in alphabet_runtime], [fmean(x) for x in alphabet_n_patterns]),
+    ([fmean(x) for x in omax_runtime], [fmean(x) for x in omax_n_patterns]),
 ]
 
-fig, axs = plt.subplots(ncols=3, sharey='all')
-cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
+fig, axs = plt.subplots(ncols=4, sharey='all')
 for (runtime, n_patterns), ax, xlabel, xticks in zip(results, axs, PARAMS, VALUES):
     twin = ax.twinx()
-    time = ax.plot(xticks, runtime, label='Time')
-    patterns = twin.plot(xticks, n_patterns, c=cycle[1], label='Patterns')
+    time = ax.plot(xticks, runtime, 'k', label='Time')
+    patterns = twin.plot(xticks, n_patterns, 'k--', label='Patterns')
     ax.set(xlabel=xlabel, xticks=xticks)
     ax.tick_params(axis='x', labelrotation=-90)
     twin.set(yticks=[], ylim=[0, 400])
@@ -62,5 +68,4 @@ twin.legend(handles=[time[0], patterns[0]])
 
 plt.savefig(join('figs', '2 params.eps'))
 plt.savefig(join('figs', '2 params.png'))
-plt.close()
 plt.close()
